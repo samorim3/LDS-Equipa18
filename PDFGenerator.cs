3cb4a9f4099829be1a97e5ef23310fbeb57f3a2e
joaopac2 <joaopac2@gmail.com>
2025-05-10 22:22:35 +0100

    Update PDFGenerator.cs

--- PDFGenerator.cs
+++ PDFGenerator.cs
@@ -1,3 +1,17 @@
+/*
+ ============================================================================
+ UC: 21178 - Laboratório de Software
+ Projeto: Sistema de Reservas de Espaços
+ Autor: João Flores (Aluno n.º 2301427)
+
+ Este código faz parte do trabalho de grupo desenvolvido na unidade curricular.
+ Integra-se com a lógica de reservas do colega Vasco Lopes (Model/Persistência).
+ 
+ Última modificação: [10/05/2025]
+
+ ============================================================================
+*/
+
 using System;
 using System.Diagnostics;
 using System.IO;
