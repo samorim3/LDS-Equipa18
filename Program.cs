2a40f6c66afcddcf2ecc2a9008c3b0703878a41b
joaopac2 <joaopac2@gmail.com>
2025-05-10 22:23:07 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
+/*
+ ============================================================================
+ UC: 21178 - Laboratório de Software
+ Projeto: Sistema de Reservas de Espaços
+ Autor: João Flores (Aluno n.º 2301427)
+
+ Este código faz parte do trabalho de grupo desenvolvido na unidade curricular.
+ Integra-se com a lógica de reservas do colega Vasco Lopes (Model/Persistência).
+ 
+ Última modificação: [10/05/2025]
+
+ ============================================================================
+*/
+
 using System;
 using System.Windows.Forms;
 
@@ -9,6 +23,8 @@ namespace ReservaEspacos
         [STAThread]
         static void Main()
         {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
             // Define configurações visuais da aplicação
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
